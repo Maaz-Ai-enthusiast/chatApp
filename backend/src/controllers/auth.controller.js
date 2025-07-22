5b5c725066121ccc415b955ad5d764779f991eb1
Maaz-Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2025-07-22 23:20:39 +0500

    pending jwt implementation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,38 @@
+import User from "../models/User";
 export async function signup(req, res) {
+    const {email, password, fullName} = req.body;
+   try {
+    if (!email || !password || !fullName) {
+        return res.status(400).json({message: 'All fields are required'});
+    }
+    if (password.length < 6) {
+        return res.status(400).json({message: 'Password must be at least 6 characters long'});
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({message: 'Invalid email format'});
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).json({message: 'Email already exists'});
+    }
+const idx  = Math.floor(Math.random() * 100+1);
+const profileImageUrl = 'https://avatar.iran.liara.run/public/${idx}.png';
+    const newUser = new User({
+        fullName,
+        email,
+        password,
+        profilePicture: profileImageUrl
+    });
+
+    await newUser.save();
+    res.status(201).json({message: 'User registered successfully'});
+
+   } catch (error) {
+    
+   }
+
     res.send('Signup Page');
 }
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 3000;
 
