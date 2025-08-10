16703d1273822f4080a531adf60da4ca8894a8dd
Maaz-Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2025-08-10 18:31:48 +0500

    complete backend

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar  = async (req,res)=>{
 try {
@@ -31,4 +32,31 @@ export const getMessages = async (req,res)=>{
     } catch (error) {
         
     }
+}
+
+export const sendMessage = async (req,res)=>{
+
+    try {
+        const {text,image} = req.body;
+        const {id : receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url
+        } 
+        const newMessage  = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        await newMessage.save()
+        res.status(200).json(newMessage)
+    } catch (error) {
+        console.log("internal server error : ", error.message);
+    }
+
 }
